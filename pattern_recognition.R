d22170cca2ef3a0be664963226b857685b724985
Alsina <albayalsina34@gmail.com>
2019-11-22 17:04:23 +0100

    Update code

--- pattern_recognition.R
+++ pattern_recognition.R
@@ -2,14 +2,28 @@ library(OpenImageR)
 library(coop)
 
 #mnist.dat <- read.csv("mnist.csv")
-sparsity(as.matrix(mnist.dat[,2:ncol(mnist.dat)]))
 
+# First question
+sparsity(as.matrix(mnist.dat[,2:ncol(mnist.dat)]))
 means <- c()
-for(i in 2:ncol(mnist.dat)){
-   means[i-1] <- mean(mnist.dat[,i])
-}
-# 
+means <- colMeans(mnist.dat[,2:ncol(mnist.dat)])
+
 imageShow(matrix(as.numeric(means),nrow=28,ncol=28,byrow=T))
 label_counts <- summary(as.factor(mnist.dat[,1]))
 majority_label <- label_counts[order(label_counts, decreasing = TRUE)]
 error_majority_label <- (nrow(mnist.dat) - as.numeric(majority_label[1])) / nrow(mnist.dat)
+
+## Second question
+ink_cost <- c()
+ink_cost <- rowSums(mnist.dat[,2:ncol(mnist.dat)])
+ink_mean <- rowMeans(mnist.dat[,2:ncol(mnist.dat)])
+ink_sd <- apply(mnist.dat[,2:ncol(mnist.dat)], 1, sd)
+
+
+## Third question: density (ink_cost) + sparsity
+
+  
+  
+  
+  
+  
